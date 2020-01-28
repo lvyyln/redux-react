7216a830a6999501cff755b3c6cd2bd9a361e9b9
DESKTOP-MATMQ38\lvyyl <denus02@ukr.net>
2020-01-28 14:18:08 +0200

    Project structure

---
+++ reactredux/src/components/spinner/spinner.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import './spinner.css'
+
+export default class Spinner extends Component {
+    render() {
+        return (
+            <div>
+                <div className="loadingio-spinner-cube-5j2d8uwiplo">
+                    <div className="ldio-gk6mkfq24cm">
+                        <div>
+
+                        </div>
+                        <div>
+
+                        </div>
+                        <div>
+
+                        </div>
+                        <div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
